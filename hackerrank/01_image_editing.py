2d68baee9158282b937373351fb0b5a9d6df8cbd
Alexey Demin <deminn@gmail.com>
2019-12-23 22:28:33 -0600

    First Max Square Solution

--- hackerrank/01_image_editing.py
+++ hackerrank/01_image_editing.py
@@ -7,24 +7,41 @@ class Solution:
             for x, el in enumerate(line):
                 if el > 0:
                     max_current += 1
-                    print(max_current)
-                    # if Solution.isSquare(self, ar, x, y, max_current):
-                    #     max_sq = max(max_sq, max_current)
+                    print('max_current=' + str(max_current))
+                    if Solution.isSquare(self, ar, x - max_current + 1, y, max_current):
+                        max_sq = max(max_sq, max_current)
                 else:
                     max_current = 0
             max_current = 0
-            print('======')
+            print('===line ' + str(y+1) +' ===')
         return max_sq
 
     def isSquare(self, ar, x, y, le):
+        if le == 1:
+            return True
         print(x, y, le)
         res = 1
         for i in range(x, x + le):
             for j in range(y, y + le):
-                res *= arr[i][j]
-        #print(res)
-        return res
+                if y + le > len(arr[i]):
+                    return False
+                print('---i=' + str(i) + ';j=' + str(j) + ';a=' + str(ar[i][j]) + ';---')
+                res *= ar[i][j]
+        print(str(le) + 'is_square=' + str(res))
+        return bool(res)
 
+
+arr = [
+    # 0  1  2  3
+    [1, 1, 1, 1],  # 0
+    [1, 1, 1, 1],  # 1
+    [1, 1, 1, 1],  # 2
+    [1, 0, 1, 1],  # 3
+]
+a = Solution.largestMatrix(None, arr)
+
+print('==========================')
+print(a)
 
 # arr = [
 #     [1, 1, 1, 1, 1],
@@ -33,22 +50,3 @@ class Solution:
 #     [1, 1, 1, 1, 1],
 #     [0, 1, 1, 1, 1],
 # ]
-arr = [
-    [0, 0, 0, 1],
-    [0, 1, 1, 1],
-    [0, 1, 1, 1],
-    [1, 1, 1, 0],
-    [1, 0, 0, 1],
-]
-a = Solution.largestMatrix(None, arr)
-#a = Solution.isSquare(None, arr, 2, 2, 3)
-# print('===')
-print(a)
-
-# IV = 4
-# IX = 9
-# XL
-# XC
-# CD
-# CM
-#
