fd68cc2fe742742647ec83e7d300e4fee3e96a82
Alexey Demin <deminn@gmail.com>
2019-12-24 18:20:51 -0600

    Max Square Solution

--- hackerrank/01_image_editing.py
+++ hackerrank/01_image_editing.py
@@ -3,45 +3,56 @@ class Solution:
         max_current = 0
         max_sq = 0
         for y, line in enumerate(ar):
-            # print(y, line)
+            #print('===line ' + str(y) + ' ===')
             for x, el in enumerate(line):
                 if el > 0:
                     max_current += 1
-                    print('max_current=' + str(max_current))
+                    #print('max_current=' + str(max_current))
                     if Solution.isSquare(self, ar, x - max_current + 1, y, max_current):
                         max_sq = max(max_sq, max_current)
                 else:
                     max_current = 0
             max_current = 0
-            print('===line ' + str(y+1) +' ===')
+
         return max_sq
 
     def isSquare(self, ar, x, y, le):
         if le == 1:
             return True
-        print(x, y, le)
+        #print('> Checking if it is square: element(' + str(x) + ',' + str(y) + ')=' + str(ar[x][y]) + '; length=' + str(le))
         res = 1
-        for i in range(x, x + le):
-            for j in range(y, y + le):
-                if y + le > len(arr[i]):
+        for i in range(y, y + le):
+            if y + le > len(arr):
+                return False
+            for j in range(x, x + le):
+                if x + le > len(arr[i]):
                     return False
-                print('---i=' + str(i) + ';j=' + str(j) + ';a=' + str(ar[i][j]) + ';---')
-                res *= ar[i][j]
-        print(str(le) + 'is_square=' + str(res))
+                #print('  element(' + str(j) + ',' + str(i) + ')=' + str(ar[i][j]))
+                res *= int(ar[i][j])
+        #print('  is_square=' + str(res))
         return bool(res)
 
 
-arr = [
+arr1 = [
     # 0  1  2  3
     [1, 1, 1, 1],  # 0
     [1, 1, 1, 1],  # 1
     [1, 1, 1, 1],  # 2
     [1, 0, 1, 1],  # 3
 ]
+# arr = [[1, 0, 1, 0, 0], [1, 0, 1, 1, 1], [1, 1, 1, 1, 1], [1, 0, 0, 1, 0]]
+
+arr = [
+   # 0  1  2  3
+    [1, 0, 1, 0, 0],   # 0
+    [1, 0, 1, 5, 1],   # 1
+    [1, 1, 1, 1, 1],   # 2
+    [1, 0, 1, 1, 0]    # 3
+]
 a = Solution.largestMatrix(None, arr)
 
-print('==========================')
-print(a)
+#print('==========================')
+#print(a)
 
 # arr = [
 #     [1, 1, 1, 1, 1],
