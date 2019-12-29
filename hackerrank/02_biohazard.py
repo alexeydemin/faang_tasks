5968e4e14977b2273ae7f398b30fab5807b6924f
Alexey Demin <deminn@gmail.com>
2019-12-28 21:19:54 -0600

    Biohazard work buy fails 3 test cases

--- hackerrank/02_biohazard.py
+++ hackerrank/02_biohazard.py
@@ -1,21 +1,48 @@
-class Solution:
-    @staticmethod
-    def bio_hazard(self, n, allergic, poisonous):
-        count = 0
-        a = []
-        for i in range(1, n + 1):
-            print('---step #' + str(i) + '---')
-            k = []
-            for elem in a:
-                elem_with_i = elem + [i]
-                k.append(elem_with_i)
-            a = [*a, *k]
-            a.append([i])
-            print(a)
-            print('---endstep ---')
+def bioHazard(self, n, allergic, poisonous):
+    rules1 = get_rules(None, allergic, poisonous)
+    rules2 = get_rules(None, poisonous, allergic)
+    count = 0
+    a = []
+    for i in range(1, n + 1):
+        #print('=== step' + str(i) + '===')
+        temp = []
+        for elem in a:
+            if is_compatible(None, elem, i, rules1):
+                if is_compatible(None, elem, i, rules2):
+                    temp.append(elem + [i])
+                    count += 1
+        a = temp
+        a.append([i])
+        count += 1
+        print(a)
+        #print('=== end step' + str(i) + '===')
+    return count
 
-        return a
+
+def is_compatible(self, elem, i, rules):
+    if i not in rules:
+        return True
+    for al in rules[i]:
+        if al in elem:
+            return False
+    return True
 
 
-res = Solution.bio_hazard(None, 5, [], [])
-# print(res)
+def get_rules(self, allergic, poisonous):
+    res = {}
+    for i, p in enumerate(poisonous):
+        if p in res:
+            res[p].append(allergic[i])
+        else:
+            res[p] = [allergic[i]]
+    return res
+
+
+res = bioHazard(None, 5, [2], [2])
+print(res)
+#res = bioHazard(None, 4, [1, 2], [3, 4])
+#print(res)
+
+#res = bioHazard(None, 5, [1, 2], [3, 5])
+
+#print(res)
